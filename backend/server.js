810b2bd8e8e20d0e36b8fdcad113e5d34f696cc7
gashaw mucheye <gashaw2533@gmail.com>
2025-09-26 15:58:14 +0300

    created login_branch and implement login logic

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 import express from 'express';
 import connectDb from './lib/db.js';
 import ENV from './lib/env.js';
+import cookieParser from 'cookie-parser';
+
 const app = express();
 connectDb();
-
+import authRoutes from './routes/authRoutes.js';
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 const PORT = ENV.PORT || 8080;
-import authRoutes from './routes/authRoutes.js';
 
 app.use('/auth', authRoutes);
 
